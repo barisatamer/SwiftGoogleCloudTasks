75a3409eb39d02ecacb4cca5e58b1647b3217658
Baris Atamer <brsatamer@gmail.com>
2020-04-05 02:52:24 +0200

    chore(package): update

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,23 +9,19 @@ let package = Package(
        .macOS(.v10_14)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SwiftGoogleCloudTasks",
-            targets: ["SwiftGoogleCloudTasks"]),
+        .library(name: "SwiftGoogleCloudTasks", targets: ["SwiftGoogleCloudTasks"]),
     ],
     dependencies: [
-        // .package(url: "https://github.com/grpc/grpc-swift.git", .branch("nio")),
-        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0-alpha.6"),
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.7.0"),
-        .package(url: "https://github.com/googleapis/google-auth-library-swift.git", from: "0.5.1")
+        .package(name: "grpc-swift", url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0-alpha.11"),
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.7.0"),
+        .package(name: "Auth", url: "https://github.com/googleapis/google-auth-library-swift.git", from: "0.5.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftGoogleCloudTasks",
-            dependencies: ["GRPC", "SwiftProtobuf", "OAuth2"]),
+        .target(name: "SwiftGoogleCloudTasks", dependencies: [
+            .product(name: "GRPC", package: "grpc-swift"), 
+            .product(name: "SwiftProtobuf", package: "SwiftProtobuf"), 
+            .product(name: "OAuth2", package: "Auth")
+        ]),
         .testTarget(
             name: "SwiftGoogleCloudTasksTests",
             dependencies: ["SwiftGoogleCloudTasks"]),
